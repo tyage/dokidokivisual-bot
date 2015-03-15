00c1c5cb0865ed64b8159aa1222057a73ac6faa4
tyage <namatyage@gmail.com>
2015-03-16 01:03:30 +0900

    not 15 but 16

--- dokidokivisual.sh
+++ dokidokivisual.sh
@@ -5,7 +5,7 @@ cd `dirname $0`
 source ./slack-config.sh
 
 CHANNEL=dokidokivisual
-declare -A MAGAZINES=([9]='まんがタイムきらら' [15]='まんがタイムきららミラク' [19]='まんがタイムきららMAX' [24]='まんがタイムきららフォワード' [28]='まんがタイムキャラット')
+declare -A MAGAZINES=([9]='まんがタイムきらら' [16]='まんがタイムきららミラク' [19]='まんがタイムきららMAX' [24]='まんがタイムきららフォワード' [28]='まんがタイムキャラット')
 
 magazine=${MAGAZINES[$(date +%d)]}
 uri="https://$SLACK_TEAM.slack.com/services/hooks/slackbot?token=$SLACK_TOKEN&channel=%23$CHANNEL"
