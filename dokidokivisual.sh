5452c66a5fcf0feb7d7eb4ff76e69b07ded51d7b
tyage <namatyage@gmail.com>
2015-10-16 15:43:38 +0900

    changed to use incoming webhook

--- dokidokivisual.sh
+++ dokidokivisual.sh
@@ -4,11 +4,10 @@
 cd `dirname $0`
 source ./slack-config.sh
 
-CHANNEL=dokidokivisual
 declare -A MAGAZINES=([9]='まんがタイムきらら' [16]='まんがタイムきららミラク' [19]='まんがタイムきららMAX' [24]='まんがタイムきららフォワード' [28]='まんがタイムキャラット')
+MAGAZINE=${MAGAZINES[$(date +%d)]}
 
-magazine=${MAGAZINES[$(date +%d)]}
-uri="https://$SLACK_TEAM.slack.com/services/hooks/slackbot?token=$SLACK_TOKEN&channel=%23$CHANNEL"
-if [ -n "$magazine" ]; then
-    curl --data "今日は$magazineの発売日です。忘れずに買いにいきましょう。" $uri
+if [ -n "$MAGAZINE" ]; then
+  MESSAGE="今日は$MAGAZINEの発売日です。忘れずに買いにいきましょう。"
+  curl -s -S -X POST --data-urlencode "payload={ \"text\": \"${MESSAGE}\" }" ${WEBHOOKURL}
 fi
